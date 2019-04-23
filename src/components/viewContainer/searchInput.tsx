b0d85b84a77ecae1f649151eba8c1911919c08b5
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-04-23 12:02:44 +0200

    new approach searchinput

---
+++ src/components/viewContainer/searchInput.tsx
@@ -0,0 +1,62 @@
+import React, { Component, CSSProperties, ChangeEvent, FormEvent } from 'react';
+import { Redirect } from 'react-router-dom';
+
+interface Props {
+    searchItem: (title: string) => void;
+}
+
+interface State {
+    searchString: string
+    searchItem: string
+    redirect: boolean
+}
+
+export default class SearchInput extends Component<Props> {
+    state: State = {
+        searchString: '',
+        searchItem: '',
+        redirect: false
+    }
+
+    onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        this.setState({ redirect: true})
+        this.props.searchItem(this.state.searchString);
+        this.setState({ searchString: '' });
+    };
+
+    onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({ searchString: event.target.value})
+
+        this.setState({ searchItem: event.target.value})
+    }
+   
+    render() {
+
+        if (this.state.redirect) {
+            return <Redirect to={`${this.state.searchItem}`} />
+        }
+        return (
+            <div>
+            <form onSubmit={this.onSubmit} style={searchBar}>
+                <input 
+                type="text"
+                name="title"
+                placeholder="Sök..."
+                value={this.state.searchString}
+                onChange={this.onChange}
+                />
+                <input
+                type="submit"
+                value="Search"
+                className="btn"
+                />
+            </form>
+            </div>
+        )
+    }
+}
+
+const searchBar: CSSProperties = {
+    margin: "auto"
+}
